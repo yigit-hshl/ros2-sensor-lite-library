07f6ad4a9d829a1630ab337d5fb1d2e6e6f1aa14
yigit-hshl <56076926+yigit-hshl@users.noreply.github.com>
2026-01-13 20:27:43 +0100

    Added predict() def for UKF

--- src/filters/ukf_filter.cpp
+++ src/filters/ukf_filter.cpp
@@ -1,16 +1,30 @@
 #include "sensor_fusion_lite/filters/ukf_filter.hpp"
 #include "sensor_fusion_lite/filters/ekf_filter.hpp" // reuse EKF functionality
+#include <cstddef>
 #include <iostream>
 
-namespace sensor_fusion_lite
-{
-  UnscentedKalmanFilter::UnscentedKalmanFilter() {}
+namespace sensor_fusion_lite {
+UnscentedKalmanFilter::UnscentedKalmanFilter() {}
 
-  void UnscentedKalmanFilter::initialize(const State& initial_state, int state_dim)
-  {
-    std::scoped_lock lock(mtx_);
-    state_ = initial_state;
-    P_.assign(state_dim, std::vector<double>(state_dim, 0.0));
-    for (int i = 0; i < std::min(state_dim, 6); ++i) P_[i][i] = 1e-3;
+void UnscentedKalmanFilter::initialize(const State &initial_state,
+                                       int state_dim) {
+  std::scoped_lock lock(mtx_);
+  state_ = initial_state;
+  P_.assign(state_dim, std::vector<double>(state_dim, 0.0));
+  for (int i = 0; i < std::min(state_dim, 6); ++i)
+    P_[i][i] = 1e-3;
+}
+
+// Temporarily copy EKF functionality
+void UnscentedKalmanFilter::predict(double dt) {
+  std::scoped_lock lock(mtx_);
+  for (int i = 0; i < 3; ++i)
+    state_.position[i] += state_.velocity[i] * dt;
+  if (!P_.empty()) {
+    double q = 1e-3 * dt;
+    for (size_t i = 0; i < P_.size(); ++i)
+      P_[i][i] += q;
   }
+  state_.timestamp = std::chrono::steady_clock::now();
+}
 } // namespace sensor_fusion_lite
